f59926dd562e8f9faddb9285bca6841381d1a948
Jack Mahoney <mahon480@morris.umn.edu>
2024-09-26 11:32:33 -0500

    Created a vowel function that returns true if a char is a vowel and false otherwise, and changed disemvowel to return a string that removes all the vowels

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,41 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "disemvowel.h"
 
+int vowel(char c) {
+    int lowercase, uppercase;
+
+    lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
+
+    if (lowercase || uppercase)
+        return true;
+    else
+        return false;
+}
+
 char *disemvowel(char *str) {
-  return (char*) "";
+  
+  
+  int constants, len, i;
+
+  len = strlen(str);
+  for (i=0; i<len; ++i) {
+    if(!vowel(str[i])) {
+      constants++;
+    }
+  }
+
+  char *result;
+  result = (char*) calloc(constants+1, sizeof(char));
+  int j = 0;
+  for (i=0; i<len; ++i) {
+    if (!vowel(str[i])) {
+      result[j++] = str[i];
+    }
+  }  
+
+  return result;
 }

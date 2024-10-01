103501179a9658909c19e7219f894ca450cf88de
Jack Mahoney <mahon480@morris.umn.edu>
2024-10-01 10:34:11 -0500

    Added comments to disemvowel

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -4,38 +4,47 @@
 
 #include "disemvowel.h"
 
+// Function to check if a character is a vowel
 int vowel(char c) {
     int lowercase, uppercase;
 
+    // Check if the character is a lowercase vowel
     lowercase = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+    // Check if the character is an uppercase vowel
     uppercase = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
 
-    if (lowercase || uppercase)
-        return true;
-    else
-        return false;
+    // Return true if the character is either a lowercase or uppercase vowel
+    return lowercase || uppercase;
 }
 
+// Function to remove vowels from a string
 char *disemvowel(char *str) {
-  
-  
-  int constants, len, i;
+    int constants = 0, len, i;
 
-  len = strlen(str);
-  for (i=0; i<len; ++i) {
-    if(!vowel(str[i])) {
-      constants++;
+    // Get the length of the input string
+    len = strlen(str);
+
+    // Count the number of non-vowel characters (consonants)
+    for (i = 0; i < len; ++i) {
+        if (!vowel(str[i])) {
+            constants++;
+        }
     }
-  }
 
-  char *result;
-  result = (char*) calloc(constants+1, sizeof(char));
-  int j = 0;
-  for (i=0; i<len; ++i) {
-    if (!vowel(str[i])) {
-      result[j++] = str[i];
+    // Allocate memory for the result string, including space for the null terminator
+    char *result = (char*) calloc(constants + 1, sizeof(char));
+    int j = 0;
+
+    // Copy non-vowel characters to the result string
+    for (i = 0; i < len; ++i) {
+        if (!vowel(str[i])) {
+            result[j++] = str[i];
+        }
     }
-  }  
 
-  return result;
+    // Null-terminate the result string
+    result[j] = '\0';
+
+    // Return the result string
+    return result;
 }
